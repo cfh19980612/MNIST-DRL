a29700349350465eac29a87e36ff115f99c7f9b5
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2021-01-02 21:28:37 +0900

    Update run.py

--- run.py
+++ run.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
             pbar.set_description("Epoch: %d Accuracy: %.3f Reward: %.3f" %(i, accuracy,reward))
                     
             # end?
-            if accuracy >= 0.95:
+            if accuracy >= 0.96.5:
                 break
             
         
