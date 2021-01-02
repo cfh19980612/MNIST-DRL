261c3934cce63a5f7ec677f2e3b918ee9a9a1474
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2021-01-02 21:14:43 +0900

    Update mnistnet.py

--- models/mnistnet.py
+++ models/mnistnet.py
@@ -26,7 +26,6 @@ class MNISTNet(nn.Module):
         layers['out'] = nn.Linear(current_dims, n_class)
 
         self.model= nn.Sequential(layers)
-        print(self.model)
 
     def forward(self, input):
         input = input.view(input.size(0), -1)
