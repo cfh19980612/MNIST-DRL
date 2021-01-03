4dc3725e673daf5742265a80b774bd1f02733112
Fahao Chen <chenfh612@gmail.com>
2021-01-03 20:36:00 +0900

    Update

--- Network.py
+++ Network.py
@@ -72,4 +72,4 @@ class Critic(nn.Module):
         x = F.leaky_relu(self.fc2(x))
         x = F.leaky_relu(self.fc3(x))
         
-        return sigmoid(self.fc4(x))
+        return F.relu(self.fc4(x))
