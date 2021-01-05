645dfb45302719052ad02e35f40be82e1334eec7
cfh19980612 <56435427+cfh19980612@users.noreply.github.com>
2021-01-05 20:26:03 +0900

    Update DDPG.py

--- DDPG.py
+++ DDPG.py
@@ -16,8 +16,8 @@ BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-3         # learning rate of the actor 
-LR_CRITIC = 3e-3        # learning rate of the critic
+LR_ACTOR = 1e-4         # learning rate of the actor 
+LR_CRITIC = 3e-4        # learning rate of the critic
 WEIGHT_DECAY = 0.0001        # L2 weight decay
 
 
